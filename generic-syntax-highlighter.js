a2a5c51480d4f122c4f156d810f4ad1ef1f819e0
Taufik Nurrohman <t.nurrohman77@gmail.com>
2016-12-19 22:42:45 +0700

    Update

--- generic-syntax-highlighter.js
+++ generic-syntax-highlighter.js
@@ -3,13 +3,30 @@
 (function(win, doc) {
 
     function SH(s) {
-        return '<span style="color:#000000">' + s.replace(/(?:<.*?>)|(?:&lt;\?\S*|\?&gt;)|(?:&lt;.*?&gt;)|(?:&lt;!\-\-[\s\S]*?\-\-&gt;|\/\/[^\n]+|#\s+[^\n]+|\/\*[\s\S]*\*\/)|(?:"(?:\\.|[^"\n])*"|'(?:\\.|[^'\n])*'|`(?:\\.|[^`])*`|\/(?:\\\/|.)+\/[gimuy]*)|&amp;\S+;|(?:\b(?:true|false)\b)|(?:(?:\d*\.)?\d+)|\b(?:a(?:bstract|lias|nd|rguments|rray|s(?:m|sert)?|uto)|b(?:ase|egin|ool(?:ean)?|reak|yte)|c(?:ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|de(?:bugger|cimal|clare|f(?:ault|er)?|init|l(?:egate|ete)?)|do|double|e(?:cho|ls?if|lse(?:if)?|nd|nsure|num|vent|x(?:cept|ec|p(?:licit|ort)|te(?:nds|nsion|rn)))|f(?:allthrough|alse|inal(?:ly)?|ixed|loat|or(?:each)?|riend|rom|unc(?:tion)?)|global|goto|guard|i(?:f|mp(?:lements|licit|ort)|n(?:it|clude(?:_once)?|line|out|stanceof|t(?:erface|ernal)?)?|s)|l(?:ambda|et|ock|ong)|m(?:odule|utable)|NaN|n(?:amespace|ative|ext|ew|il|ot|ull)|o(?:bject|perator|r|ut|verride)|p(?:ackage|arams|rivate|rotected|rotocol|ublic)|r(?:aise|e(?:adonly|do|f|gister|peat|quire(?:_once)?|scue|strict|try|turn))|s(?:byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|t(?:emplate|hen|his|hrows?|ransient|rue|ry|ype(?:alias|def|id|name|of))|u(?:n(?:checked|def(?:ined)?|ion|less|signed|til)|se|sing)|v(?:ar|irtual|oid|olatile)|w(?:char_t|hen|here|hile|ith)|xor|yield)\b/g, function(a) {
+        return '<span style="color:#000000">' + s.replace(new RegExp([
+            '<.*?>', // "real" tags
+            '&lt;\\?\\S*', // template open
+            '\\?&gt;', // template close
+            '&lt;.*?&gt;', // HTML tags
+            '&lt;!\\-\\-[\\s\\S]*?\\-\\-&gt;', // HTML comments
+            '\\/\\/[^\\n]+', // comments
+            '#\\s+[^\\n]+', // comments
+            '\\/\\*[\\s\\S]*\\*\\/', // comments
+            '"(?:\\\\.|[^"\\n])*"', // strings
+            '\'(?:\\\\.|[^\'\\n])*\'', // strings
+            '`(?:\\\\.|[^`])*`', // strings
+            '\\/[^\\n]+\\/[gimuy]*', // regular expressions
+            '&amp;\\S+;', // entities
+            '\\b(?:true|false)\\b', // booleans
+            '(?:\\d*\.)?\\d+', // numbers
+            '\\b(?:a(?:bstract|lias|nd|rguments|rray|s(?:m|sert)?|uto)|b(?:ase|egin|ool(?:ean)?|reak|yte)|c(?:ase|atch|har|hecked|lass|lone|ompl|onst|ontinue)|de(?:bugger|cimal|clare|f(?:ault|er)?|init|l(?:egate|ete)?)|do|double|e(?:cho|ls?if|lse(?:if)?|nd|nsure|num|vent|x(?:cept|ec|p(?:licit|ort)|te(?:nds|nsion|rn)))|f(?:allthrough|alse|inal(?:ly)?|ixed|loat|or(?:each)?|riend|rom|unc(?:tion)?)|global|goto|guard|i(?:f|mp(?:lements|licit|ort)|n(?:it|clude(?:_once)?|line|out|stanceof|t(?:erface|ernal)?)?|s)|l(?:ambda|et|ock|ong)|m(?:odule|utable)|NaN|n(?:amespace|ative|ext|ew|il|ot|ull)|o(?:bject|perator|r|ut|verride)|p(?:ackage|arams|rivate|rotected|rotocol|ublic)|r(?:aise|e(?:adonly|do|f|gister|peat|quire(?:_once)?|scue|strict|try|turn))|s(?:byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch)|t(?:emplate|hen|his|hrows?|ransient|rue|ry|ype(?:alias|def|id|name|of))|u(?:n(?:checked|def(?:ined)?|ion|less|signed|til)|se|sing)|v(?:ar|irtual|oid|olatile)|w(?:char_t|hen|here|hile|ith)|xor|yield)\\b' // keywords
+        ].join('|'), 'g'), function(a) {
             if (a) {
-                if (/^(\W|(<.*?>))$/.test(a)) {
-                    // skip punctuations and tags ...
+                if (/^(\W|<.*?>)$/.test(a)) {
+                    // skip punctuations and "real" tags ...
                 } else if (/^(&lt;!\-\-)/.test(a) || /^&lt;\?\S*|\?&gt;$/.test(a)) {
                     a = '<span style="color:#008000;font-style:italic;">' + a + '</span>'; // HTML comments and templates
-                } else if (/^(\/\/|# |\/\*)/.test(a)) {
+                } else if (/^(\/\/|#\s+|\/\*)/.test(a)) {
                     a = '<span style="color:#808080;font-style:italic;">' + a + '</span>'; // comments
                 } else if (/^&lt;!/.test(a)) {
                     a = '<span style="color:#4682B4;font-style:italic;">' + a + '</span>'; // document types
